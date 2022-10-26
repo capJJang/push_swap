aa573cb4d2b7252f3bd69cd0bcc1b7878e4219b7
Segun An <segan@c7r6s4.42seoul.kr>
2022-10-26 20:28:24 +0900

    lis implementation is almost over...

--- make_lis.c
+++ make_lis.c
@@ -6,11 +6,68 @@
 /*   By: segan <segan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 17:32:55 by segan             #+#    #+#             */
-/*   Updated: 2022/10/25 19:43:44 by segan            ###   ########.fr       */
+/*   Updated: 2022/10/26 20:26:39 by segan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
+
+int	*get_real_lis(int *lis_index, int *dp, long *stack_a)
+{
+	int	i;
+	int	max;
+	int	from;
+	int	j;
+	int	*real_lis;
+
+	i = 0;
+	max = 0;
+	real_lis = (int *)malloc(sizeof(dp) / sizeof(*dp));
+	while (i < sizeof(dp) / sizeof(*dp))
+	{
+		if (dp[i] > max)
+			max = dp[i];
+		i++;
+	}
+	while (max != 0)
+	{
+		from = get_index(dp, max);
+		real_lis[max] = stack_a[from];
+	}
+	real_lis[0] = stack_a[0];
+	free_arr(lis_index, dp);
+	return (real_lis);
+}
+
+//dp배열 내부의 최대값을 찾음
+//최대값
+
+int	*get_lis(long *stack_a)
+{
+	int		i;
+	int		j;
+	long	*lis_index;
+	int		*dp;
+
+	lis_index = (int *)malloc(sizeof(int) * sizeof(stack_a) / sizeof(*stack_a));
+	dp = (int *)malloc(sizeof(int) * sizeof(stack_a) / sizeof(*stack_a));
+	i = -1;
+	while (++i < sizeof(stack_a) / sizeof(*stack_a))
+	{
+		j = -1;
+		dp[i] = 1;
+		while (++j < i)
+		{
+			if (stack_a[j] < stack_a[i])
+			{
+				dp[i] = max(dp[i], dp[j] + 1);
+				lis_index[i] = j;
+			}
+		}
+	}
+	return (get_real_lis(lis_index, dp, stack_a));
+}
 
 void	pull_min_val_to_top(long *stack_a)
 {
@@ -40,37 +97,13 @@ void	pull_min_val_to_top(long *stack_a)
 			ra(stack_a);
 }
 
-long	*get_lis(long *stack_a)
-{
-	int		i;
-	int		k;
-	long	*lis;
-	int		*record;
-	int		size;
-
-	size = stacksize(stack_a);
-	lis = (long *)malloc(sizeof(long) * size);
-	record = (int *)malloc(sizeof(int) * size);
-	k = 0;
-	while (k < size)
-	{
-		i = 0;
-		record[k] = 1;
-		while (i < k)
-		{
-			if (stack_a[i] < stack_a[k])
-			record[k] = max(record[k], record[i] + 1);
-			i++;
-		}
-		k++;
-	}
-}
-
 long	*make_lis(long *stack_a)
 {
-	long	*lis;
+	int	*lis;
 
 	pull_min_val_to_top(stack_a);
 	lis = get_lis(stack_a);
+	for(int i = 0; i < sizeof(lis) / sizeof(*lis); i++)
+		printf("%d \t", lis[i]);
 	return (stack_a);
 }
