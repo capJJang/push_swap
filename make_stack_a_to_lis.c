0d3ab5f4609535be2e0652b3bd88d766c255a2a9
haha <dkstprjs@naver.com>
2022-10-29 03:30:41 +0900

    add is_sorted function

--- make_stack_a_to_lis.c
+++ make_stack_a_to_lis.c
@@ -6,7 +6,7 @@
 /*   By: segan <segan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 14:39:23 by segan             #+#    #+#             */
-/*   Updated: 2022/10/27 17:34:31 by segan            ###   ########.fr       */
+/*   Updated: 2022/10/29 03:24:57 by segan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	make_stack_a_to_lis(long *stack_a, long *stack_b, long *lis)
 
 	stack_a_size = stacksize(stack_a);
 	lis_size = stacksize(lis);
+	if (is_sorted(stack_a) == 1)
+		return (free_arr(lis, NULL));
 	while (stack_a_size)
 	{
 		if (*stack_a == *lis)
@@ -32,4 +34,20 @@ void	make_stack_a_to_lis(long *stack_a, long *stack_b, long *lis)
 		stack_a_size--;
 	}
 	free_arr(lis, NULL);
+}
+
+int	is_sorted(long *stack)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = stacksize(stack);
+	while (i < size - 1)
+	{
+		if (stack[i] > stack[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
 }
